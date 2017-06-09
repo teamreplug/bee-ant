166608de404f703ed5cae6a48a78c2ae3430cbb6
DESKTOP-TL741B2\IM <emmanuel.adeojo@yahoo.com>
2017-06-09 21:17:35 +0100

    new changes to app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
